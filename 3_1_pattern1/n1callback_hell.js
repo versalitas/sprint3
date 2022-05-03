683262ea8bc6f1087c3bd94dce2216801a068f4a
versalitas <100954079+versalitas@users.noreply.github.com>
2022-05-03 10:19:45 +0200

    Polish code

--- 3_1_pattern1/n1callback_hell.js
+++ 3_1_pattern1/n1callback_hell.js
@@ -3,47 +3,17 @@
 //contingut invertit en un altre fitxer al directori outbox. 
 //Reestructura i simplifiqui el codi existent per a evitar el denominat Callback Hell.
 
-// Node.js program to illustrate 
-// util.promisify() methods
-  
-// Since promisify function 
-// returns promise version 
-// of a function, it can also
-// operate using async and await
-   
-// Importing Utilities module
-//const util = require('util')
-   
-// Importing File System module
-//const fs = require('fs')
-   
-// Use promisify to convert callback
-// based method fs.readdir to 
-// promise based method
-//const readdir = util.promisify(fs.readdir)
-   
-//const readFiles = async (path) => {
-//    const files = await readdir(path)
-//    console.log(files)
-//  }
-   
-//  readFiles(process.cwd()).catch(err => {
-//    console.log(err)
-//  })
-
-// https://javascript.info/async-await
-//https://www.geeksforgeeks.org/node-js-util-promisify-method/?ref=gcse
-
-const {promisify} = require('util');
-
+const {promisify} = require('utils');
 const {
   readdir,
   readFile,
   writeFile
 } = require("fs");
+
 const {
   join
 } = require("path");
+
 const inbox = join(__dirname, "inbox");
 const outbox = join(__dirname, "outbox");
 
@@ -69,19 +39,24 @@ readdir(inbox, (error, files) => {
 });
 */
 
-const resReaddir = util.promisify(readdir);
-const resReadFile = util.promisify(readFile);
-const resWriteFile = util.promisify(writeFile);
-
-const reverseInNewFile = async (inbox, outbox, files) => {
-  
-    const files = await resReaddir(inbox);
-    files.forEach(file => {
-      const readFile = await resReadFile(join (inbox, file), "utf8");
-      const reverseFile = await reverseText(readFile);
-      const result = writeFile(join(outbox, file), reverseFile);
-      console.log("${reverseFile} was successfully saved in the outbox!");
-    }
- 
+const promReaddir = util.promisify(readdir);
+const promReadFile = util.promisify(readFile);
+const promWriteFile = util.promisify(writeFile);
 
 
+reverseReadSave = async () => {
+  try {
+      const files = await promReaddir(inbox);
+      for await (const file of files) {
+          const readData = await promReadFile(join(inbox, file), 'utf8');
+          const reverseData = await reverseText(readData);
+          await promWriteFile(join(outbox, file), reverseData);
+          console.log(`${file} done`);
+      }
+  } catch (error) {
+      console.log(error)
+  }
+}
+
+
+readReverseSave();
