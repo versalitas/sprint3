b9adffdd3a134d5e434b1912e4bf5738a37c8013
versalitas <100954079+versalitas@users.noreply.github.com>
2022-05-10 09:21:50 +0200

    changed name from index.js to app.js

---
+++ callbackHell/app.js
@@ -0,0 +1,48 @@
+
+//El codi adjunt llegeix un fitxer situat en un directori inbox i escriu el seu 
+//contingut invertit en un altre fitxer al directori outbox. 
+//Reestructura i simplifiqui el codi existent per a evitar el denominat Callback Hell.
+
+
+
+
+const {promisify} = require('util');
+const {readdir, readFile, writeFile} = require("fs");
+
+const {join} = require("path");
+
+const inbox = join(__dirname, "inbox");
+const outbox = join(__dirname, "outbox");
+
+const reverseText = str =>
+  str
+  .split("")
+  .reverse()
+  .join("");
+
+ 
+// en sprint 1 me topé con promisify. 
+// muy cómodo pero no sé cómo mantener
+// los mensajes de error más diversificados
+// del resultado
+const pReaddir = promisify(readdir);
+const pReadFile = promisify(readFile);
+const pWriteFile = promisify(writeFile);
+
+
+
+const reverseReadSave = async () => {
+  try {
+      const files = await pReaddir(inbox);
+      for (let file of files) {
+          const readData = await pReadFile(join(inbox, file), 'utf8');
+          await pWriteFile(join(outbox, file), reverseText(readData), "utf8");
+          console.log(`${file} reversed and saved in the outbox.`);
+      }
+  } catch (err) {
+      console.log("Some highly expected error.", err.message);
+  }
+}
+
+
+reverseReadSave();
