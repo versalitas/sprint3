a038125b5534ca70f986a5040e42cbafa6d0f900
MK <caprichismo@gmail.com>
2022-05-10 19:21:55 +0200

    Mowing around everything

--- decorator/app.js
+++ decorator/app.js
@@ -1,1 +1,19 @@
-//require(./middleware)
+
+const ShoppingCart = require('./shoppingCart');
+
+const shoppingCart = new ShoppingCart();
+
+shoppingCart.addToCart();
+shoppingCart.convertToEuro();
+shoppingCart.
+
+   
+    
+
+
+
+
+
+
+
+
