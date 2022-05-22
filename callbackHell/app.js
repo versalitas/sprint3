89c5483d041b8d4c41691e602d46fb2129a8b83a
MK <caprichismo@gmail.com>
2022-05-22 08:48:12 +0200

    add initial refactoring to promises

--- callbackHell/app.js
+++ callbackHell/app.js
@@ -1,5 +1,47 @@
+//refactoring with promises from sprint 1
+const {readdir, readFile, writeFile} = require("fs");
 
-//El codi adjunt llegeix un fitxer situat en un directori inbox i escriu el seu 
+const {join} = require("path");
+
+const inbox = join(__dirname, "inbox");
+const outbox = join(__dirname, "outbox");
+
+const reverseText = str =>
+  str
+  .split("")
+  .reverse()
+  .join("");
+
+
+const promReadDir = (inbox) => {
+  return new Promise((resolve, rejec) => {
+    readdir(inbox), err, files) => {
+      if(err) rej("Error: Folder inaccessible");
+      res
+    }
+  })
+}
+
+
+
+/* original callbackhell
+// Read and reverse contents of text files in a directory
+readdir(inbox, (error, files) => {
+  if (error) return console.log("Error: Folder inaccessible");
+  files.forEach(file => {
+    readFile(join(inbox, file), "utf8", (error, data) => {
+      if (error) return console.log("Error: File error");
+      writeFile(join(outbox, file), reverseText(data), error => {
+        if (error) return console.log("Error: File could not be saved!");
+        console.log(`${file} was successfully saved in the outbox!`);
+      });
+    });
+  });
+});
+
+*/
+
+/*//El codi adjunt llegeix un fitxer situat en un directori inbox i escriu el seu 
 //contingut invertit en un altre fitxer al directori outbox. 
 //Reestructura i simplifiqui el codi existent per a evitar el denominat Callback Hell.
 
@@ -46,3 +88,4 @@ const reverseReadSave = async () => {
 
 
 reverseReadSave();
+*/
