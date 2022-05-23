a59eaee3843c685da97b76a3625b08fc5c71142e
MK <caprichismo@gmail.com>
2022-05-23 16:41:46 +0200

    add another try

--- callbackHell/app.js
+++ callbackHell/app.js
@@ -1,5 +1,5 @@
 
-///aaargggggg!!!!!
+///AAAAArggg...
 
 const {readdir, readFile, writeFile} = require("fs");
 
@@ -15,6 +15,8 @@ const reverseText = str =>
   .join("");
 
 
+  
+/*
 const getFiles = () => {
 return new Promise(function(resolve,reject){
   readdir(inbox, (err,files) =>{
@@ -22,13 +24,20 @@ return new Promise(function(resolve,reject){
     else resolve(files)
   })
  })
+}*/
+const getFiles = () => {
+  
+
 }
 
 const readReverseWrite = (files) => {
-  for(let file in files) {
+  for(let file of files) {
+  console.log(files);
+  
   readFile(join(inbox, file), "utf8", (err, data) => {
   if (err) {return console.log("err: File err");
   } else { 
+  //write reversed text to file
   writeFile(join(outbox, file), reverseText(data), err => {
     if (err) return console.log("Error: File could not be saved!");
     console.log(`${file} was successfully saved in the outbox!`);
@@ -41,9 +50,10 @@ const readReverseWrite = (files) => {
 
 ///aaargggggg
 
+/*
 getFiles()
 .then(files => {
-  readReverseWrite(files)})
+  readReverseWrite(files)}) */
 
 
 
