d99a2f2a5a4d55011f2610594a9f1223396e1a69
MK <caprichismo@gmail.com>
2022-05-23 15:29:03 +0200

    add not sure what I'm doing anymore

--- callbackHell/app.js
+++ callbackHell/app.js
@@ -6,6 +6,22 @@ const {join} = require("path");
 const inbox = join(__dirname, "inbox");
 const outbox = join(__dirname, "outbox");
 
+
+const getFiles = () =>
+{
+  readdir(inbox,(err,files) => {
+    if (err) return console.log("err: Folder inaccessible");
+    return files;
+  })
+}
+
+const readFiles = (file) => {
+  readFile(join(inbox, file), "utf8", (err, data) => {
+  if (err) return console.log("err: File err");
+  return data;})
+  
+}
+
 const reverseText = str =>
   str
   .split("")
@@ -13,99 +29,29 @@ const reverseText = str =>
   .join("");
 
 
-  const reverseReadWrite = () => {
-     
-    let files;
-    let data;
-    let reverseData;
-    
-     //readdir
-    try {
-      files = readdir(inbox);
-    } catch (err) {
-      return console.error("Error: Folder inaccessible");
-    }
-   
-     //iterate through the files
-      files.forEach(file => {
-        
-        //read files  
-        try { 
-          data = readFile(join(inbox, file), "utf8")
-        } catch (err) {
-          return console.log("Error: File error");
-        }
-        //reverse text
-        let reverseData = reverseText(data);
-        
-        //write reversed text to file
-        try { 
-          writeFile(join(outbox, file), reverseData);
-        } catch (err) {
-          return console.log("Error: File could not be saved!");
-        }
-        
-        console.log(`${file} was successfully saved in the outbox!`);
-      }
-      
-    
-
-
+const writeFiles = (data) => {
+  writeFile(join(outbox, file), data, err => {
+  if (err) return console.log("err: File could not be saved!");
+  console.log(`${file} was successfully saved in the outbox!`);})
   
-
-
-/*
-  //readdir turned into promise resolves with the files
-const promReadDir = (inbox) => {
-  return new Promise((resolve, reject) => {
-    readdir(inbox, (err, files) => {
-      if(err) reject("Error: Folder inaccessible");
-      resolve(files);
-    });
-  });
 }
 
-// readFile turned into promise resolves reading the data from the files
-const promReadFile = (inbox, file) => {
-  return new Promise((resolve, reject) => {
-    readFile(join(inbox, file), "utf8", (err, data) => {
-      if(err) reject("Error: File error");
-      resolve(data);
-    });
-   });
+
+const readReverseWrite = () =>
+{ 
+  try{
+  let files =  getFiles();
+  console.log(files);
+ // for(let file of files){
+    let data = readFiles(files);
+    let reversedStr = reverseText(data);
+    writeFiles(reversedStr);
+    //}
+  }catch (err) {console.error(err)}
 }
 
-//writefile turned into promise resolves with message 
-const promWriteFile = (outbox, file, data) => {
-  return new Promise((resolve, reject) => {
-    writeFile(join(outbox, file), data, err => {
-      if(err) reject("Error: File could not be saved!");
-      resolve(console.log(`${file} was successfully saved in the outbox!`));
-    });
-  });
-}
 
-// puting the promises together
-
-const reverseReadWrite = async () => {
-  try {
-    let allFiles = await promReadDir(inbox);
-      for(let file of allFiles){
-        let data = await promReadFile(inbox, file);
-        let reverseData = reverseText(data);
-        await promWriteFile(outbox, file, reverseData);
-      }
-    } catch (err) {
-      console.error(err);
-  }
-}
-*/
-reverseReadWrite();
-
-
-
-
-
+readReverseWrite();
 /* original callbackhell
 // Read and reverse contents of text files in a directory
 readdir(inbox, (err, files) => {
